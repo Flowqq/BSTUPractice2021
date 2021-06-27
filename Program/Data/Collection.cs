20fc319d63c17e458dc647da4594259d4f3d12b8
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-06-27 18:02:57 +0300

    Модель набора записей в БД

---
+++ Program/Data/Collection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    public class Collection
+    {
+        public string id;
+        public string name;
+        public Dictionary<DataType, List<DataUnit>> dataUnits;
+
+        public Collection(string id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            this.dataUnits = new Dictionary<DataType, List<DataUnit>>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Collection collection &&
+                   id == collection.id &&
+                   name == collection.name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id, name);
+        }
+    }
+}
