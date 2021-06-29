35a65a9991a250c19bc8441ca326b20e85a635f9
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-06-29 20:42:15 +0300

    API для работы с файловой системой

---
+++ Program/userInterface/IFileInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Program.userInterface
+{
+    public interface IFileInterface
+    {
+        bool SaveCollection(Collection collection);
+        bool SaveDataUnit(string collectionId, DataUnit dataUnit);
+        bool UpdateDataUnit(string collectionId, DataUnit dataUnit);
+        bool DeleteDataUnit(string collectionId, string dataUnitId);
+        SortedSet<DataUnit> SearchDataUnits(string collectionId, SortedSet<DataUnit> searchFields);
+    }
+}
