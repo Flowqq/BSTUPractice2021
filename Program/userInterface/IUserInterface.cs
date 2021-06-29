a41dcc1ecdf12ec69798ddc9035a9d9c080af634
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-06-29 20:40:29 +0300

    API для интерфейса пользовавтеля

---
+++ Program/userInterface/IUserInterface.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Program.userInterface
+{
+    public interface IUserInterface
+    {
+        Collection AddCollection(string collectionName);
+        Dictionary<DataType, SortedSet<DataUnit>> GetCollectionData(string collectionId);
+        DataUnit AddDataUnit(string collectionId, DataUnit dataUnit);
+        DataUnit UpdateDataUnit(string collectionId, string dataUnitId, SortedSet<DataUnitProp> updatedProps);
+        bool DeleteDataUnit(string collectionId, string dataUnitId);
+        SortedSet<DataUnit> SearchDataUnits(string collectionId, SortedSet<DataUnit> searchFields);
+    }
+}
