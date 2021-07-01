c0b45e1aebce405f57f3253e90ab0c5f72ece48b
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-07-01 23:43:26 +0300

    Добавил методы для работы с записью

--- Program/Data/DataUnit.cs
+++ Program/Data/DataUnit.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Program
@@ -7,7 +6,7 @@ namespace Program
     public class DataUnit
     {
         public string Id { get; }
-        public SortedSet<DataUnitProp> Props { get; set; }
+        public SortedSet<DataUnitProp> Props { get; }
 
         public DataUnit(string id)
         {
@@ -15,11 +14,58 @@ namespace Program
             Props = new SortedSet<DataUnitProp>();
         }
 
+        public bool Update(DataUnit newDataUnit)
+        {
+            if (newDataUnit.Id == Id)
+            {
+                foreach (var prop in newDataUnit.Props)
+                {
+                    if (!Props.Contains(prop))
+                    {
+                        AddProperty(prop);
+                    }
+                    else
+                    {
+                        UpdateProperty(prop);
+                    }
+                }
+            }
+            return false;
+        }
+
         public DataUnitProp GetProperty(string name)
         {
             return Props.FirstOrDefault(prop => prop.Name == name);
         }
 
+        public bool AddProperty(DataUnitProp dataUnitProp)
+        {
+            return Props.Add(dataUnitProp);
+        }
+
+        public bool UpdateProperty(DataUnitProp dataUnitProp)
+        {
+            return SetProperty(dataUnitProp.Name, dataUnitProp);
+        }
+        protected bool SetProperty(string name, DataUnitProp dataUnitProp)
+        {
+            var propToUpdate = GetProperty(name);
+            if (propToUpdate != null)
+            {
+                bool removed = Props.Remove(propToUpdate);
+                bool added = Props.Add(dataUnitProp);
+                if (!added)
+                {
+                    Props.Add(propToUpdate);
+                }
+                return removed && added;
+            }
+            return false;
+        }
+        protected bool Equals(DataUnit other)
+        {
+            return Id == other.Id;
+        }
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
