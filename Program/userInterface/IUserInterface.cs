388184656c494294f24951224b174df200380655
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-07-08 13:43:30 +0300

    Изменения в интерфейсе пользователя

--- Program/userInterface/IUserInterface.cs
+++ Program/userInterface/IUserInterface.cs
@@ -5,11 +5,12 @@ namespace Program.userInterface
 {
     public interface IUserInterface
     {
+        List<CollectionDefinition> GetCollectionDefinitions();
         Collection CreateCollection(string collectionName);
-        DataUnitsPaginator GetCollectionData(string collectionId);
+        DataUnitsPaginator GetCollectionData(string collectionId, int pageSize = 10);
         DataUnit AddDataUnit(string collectionId, DataUnit dataUnit);
         DataUnit UpdateDataUnit(string collectionId, string dataUnitId, SortedSet<DataUnitProp> updatedProps);
         bool DeleteDataUnit(string collectionId, string dataUnitId);
-        DataUnitsPaginator SearchDataUnits(string collectionId, SortedSet<DataUnitProp> searchFields);
+        DataUnitsPaginator SearchDataUnits(string collectionId, SortedSet<DataUnitProp> searchFields, int pageSize = 10);
     }
 }
