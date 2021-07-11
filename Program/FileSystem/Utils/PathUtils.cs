e862d22679c446b1d05e0eb3a7555f3275a83f03
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-07-11 19:09:58 +0300

    Добавил понятные Util классы

---
+++ Program/FileSystem/Utils/PathUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program.Utils
+{
+    public class PathUtils
+    {
+        public static string GetCollectionIndexFilepath(CollectionDefinition collectionDefinition)
+        {
+            return $"{FileSystemConfig.COLLECTIONS_FILEPATH}{collectionDefinition.Id}/{FileSystemConfig.INDEX_FILENAME}";
+        }
+
+        public static string GetCollectionDataFilepath(string collectionId)
+        {
+            return $"{FileSystemConfig.COLLECTIONS_FILEPATH}{collectionId}/";
+        }
+
+        public static string GetCollectionDataFilepathByIndex(IdIndex index)
+        {
+            if (index.IsLeaf)
+            {
+                var indexColDef = index.ColDef;
+                return $"{GetCollectionDataFilepath(indexColDef.Id)}{index.FileName}{FileSystemConfig.INDEX_FILE_POSTFIX}{FileSystemConfig.FILE_EXTENSION}";
+            }
+            throw new Exception("This index isn't leaf index tree!");
+        }
+    }
+}
