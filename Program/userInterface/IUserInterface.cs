82e4a38f7cc94a11c97fe633807677b9afa5cad9
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2021-07-13 23:49:21 +0300

    Изменил интерфейс для пользователя

--- Program/userInterface/IUserInterface.cs
+++ Program/userInterface/IUserInterface.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Program.DataPage;
 
 namespace Program.userInterface
@@ -10,10 +11,12 @@ namespace Program.userInterface
         CollectionDefinition CreateCollection(string collectionName);
         CollectionDefinition RenameCollection(string collectionId, string newName);
         void DeleteCollection(string collectionId);
-        DataUnitsPaginator GetCollectionData(string collectionId, int pageSize = 10);
+        DataUnitsPaginator GetCollectionData(string collectionId, Comparison<DataUnit> sortFunc = null, int pageSize = 10);
+        DataUnitsPaginator SearchDataUnits(string collectionId, List<DataUnitProp> searchFields, Comparison<DataUnit> sortFunc = null, int pageSize = 10);
+        DataUnitsPaginator SearchDataUnitsAllCollections(List<DataUnitProp> searchFields, Comparison<DataUnit> sortFunc = null, int pageSize = 10);
+
         DataUnit AddDataUnit(string collectionId, List<DataUnitProp> props);
         DataUnit UpdateDataUnit(string collectionId, DataUnit dataUnit);
         void DeleteDataUnit(string collectionId, string dataUnitId);
-        DataUnitsPaginator SearchDataUnits(string collectionId, List<DataUnitProp> searchFields, int pageSize = 10);
     }
 }
